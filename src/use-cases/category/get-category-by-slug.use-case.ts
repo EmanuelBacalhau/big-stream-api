fa5dd50b53b378a231b3b3f091f264156dc92e44
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-08-01 12:01:35 -0300

    refactor: add refactoring bug in use case for get category by slug

--- src/use-cases/category/get-category-by-slug.use-case.ts
+++ src/use-cases/category/get-category-by-slug.use-case.ts
@@ -1,10 +1,17 @@
 import type { Category } from '@prisma/client'
 import type { ICategoryRepository } from '@repositories/i-category.repository'
+import { CategoryNotFound } from '@use-cases/errors/category-not-found'
 
 export class GetCategoryBySlugUseCase {
   constructor(private readonly categoryRepository: ICategoryRepository) {}
 
   async execute(slug: string): Promise<Category | null> {
-    return await this.categoryRepository.findBySlug(slug)
+    const category = await this.categoryRepository.findBySlug(slug)
+
+    if (!category) {
+      throw new CategoryNotFound()
+    }
+
+    return category
   }
 }
